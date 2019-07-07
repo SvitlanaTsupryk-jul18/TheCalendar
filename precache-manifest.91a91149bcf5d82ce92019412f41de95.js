8ae3c9bbb534fe5f0bcf8da96fe47bdc9cc90aba
Svet <stsupryk@gmail.com>
2019-07-08 00:03:05 +0300

    Updates

---
+++ precache-manifest.91a91149bcf5d82ce92019412f41de95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5087e18e3e97f08725c12ccdb3f68de",
+    "url": "/TheCalendar/index.html"
+  },
+  {
+    "revision": "04d9bdd52fb39b029f9d",
+    "url": "/TheCalendar/static/css/main.1ae73b6b.chunk.css"
+  },
+  {
+    "revision": "0440f5c61b89d51833e3",
+    "url": "/TheCalendar/static/js/2.97294558.chunk.js"
+  },
+  {
+    "revision": "04d9bdd52fb39b029f9d",
+    "url": "/TheCalendar/static/js/main.cc81c975.chunk.js"
+  },
+  {
+    "revision": "eefc9d0c90d8577dd959",
+    "url": "/TheCalendar/static/js/runtime~main.35dc6e35.js"
+  }
+]);
